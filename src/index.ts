dfb93104b8c51f7f96a059785536f7130ce48050
Gentlee-dev <gentlee.dev@gmail.com>
2024-08-05 18:15:28 +0900

    refactor : 상수 파일 분리

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,11 @@
 import { assemble, disassemble, isCompleteHangul } from "./com.js";
-import { ALWAYS_CONVERT_KRS, EXCEPT_WORDS } from "./contant.js";
-// import { arr } from "./most.js";
+import {
+  ALWAYS_CONVERT_KRS,
+  EN_TO_KR,
+  EXCEPT_WORDS,
+  KR_TO_EN,
+} from "./contant.js";
 
-export const EN_TO_KR: { [idx: string]: string } = {
-  a: "ㅁ",
-  b: "ㅠ",
-  c: "ㅊ",
-  d: "ㅇ",
-  e: "ㄷ",
-  f: "ㄹ",
-  g: "ㅎ",
-  h: "ㅗ",
-  i: "ㅑ",
-  j: "ㅓ",
-  k: "ㅏ",
-  l: "ㅣ",
-  m: "ㅡ",
-  n: "ㅜ",
-  o: "ㅐ",
-  p: "ㅔ",
-  q: "ㅂ",
-  r: "ㄱ",
-  s: "ㄴ",
-  t: "ㅅ",
-  u: "ㅕ",
-  v: "ㅍ",
-  w: "ㅈ",
-  x: "ㅌ",
-  y: "ㅛ",
-  z: "ㅋ",
-  A: "ㅁ",
-  B: "ㅠ",
-  C: "ㅊ",
-  D: "ㅇ",
-  E: "ㄸ",
-  F: "ㄹ",
-  G: "ㅎ",
-  H: "ㅗ",
-  I: "ㅑ",
-  J: "ㅓ",
-  K: "ㅏ",
-  L: "ㅣ",
-  M: "ㅡ",
-  N: "ㅜ",
-  O: "ㅒ",
-  P: "ㅖ",
-  Q: "ㅃ",
-  R: "ㄲ",
-  S: "ㄴ",
-  T: "ㅆ",
-  U: "ㅕ",
-  V: "ㅍ",
-  W: "ㅉ",
-  X: "ㅌ",
-  Y: "ㅛ",
-  Z: "ㅋ",
-};
-export const KR_TO_EN: { [idx: string]: string } = {
-  ㅁ: "a",
-  ㅠ: "b",
-  ㅊ: "c",
-  ㅇ: "d",
-  ㄷ: "e",
-  ㄹ: "f",
-  ㅎ: "g",
-  ㅗ: "h",
-  ㅑ: "i",
-  ㅓ: "j",
-  ㅏ: "k",
-  ㅣ: "l",
-  ㅡ: "m",
-  ㅜ: "n",
-  ㅐ: "o",
-  ㅔ: "p",
-  ㅂ: "q",
-  ㄱ: "r",
-  ㄴ: "s",
-  ㅅ: "t",
-  ㅕ: "u",
-  ㅍ: "v",
-  ㅈ: "w",
-  ㅌ: "x",
-  ㅛ: "y",
-  ㅋ: "z",
-  ㄸ: "E",
-  ㅒ: "O",
-  ㅖ: "P",
-  ㅃ: "Q",
-  ㄲ: "R",
-  ㅆ: "T",
-  ㅉ: "W",
-};
 const getIsAlwaysConvertKr = (word: string) =>
   ALWAYS_CONVERT_KRS.indexOf(word) !== -1;
 const getIsCompleteKrWord = (word: string) =>
@@ -129,10 +44,9 @@ const krToEn = (word: string) => {
 };
 
 const forgotConvert = (text: string, excepts: string[] = []) => {
-  const resultString = text.replace(/[.?!,()]/g, "");
+  const resultString = text.replace(/[.?!,()]/g, ""); // 특문 제거
   const words = resultString.split(" ");
   const answer: { [idx: string]: string } = {};
-  let list = "";
 
   for (const word of words) {
     if ([...EXCEPT_WORDS, ...excepts].indexOf(word) !== -1) continue; // 예외단어 제외
@@ -142,40 +56,20 @@ const forgotConvert = (text: string, excepts: string[] = []) => {
     // 영어인 경우
     if (wordCountry === "en") {
       const convertedKr = enToKr(word); // 일단 한국어 변환
-      list += " " + convertedKr;
       if (!getIsCompleteKrWord(convertedKr)) continue; // 완성된 한글이 아니면 중단
       answer[word] = convertedKr; // 리턴객체에 삽입
     }
 
     // 한글인 경우
     if (wordCountry === "kr") {
-      if (!getIsAlwaysConvertKr(word) && getIsCompleteKrWord(word)) continue; // 완성된 한글이면 중단
+      if (!getIsAlwaysConvertKr(word) && getIsCompleteKrWord(word)) continue; // 필수번역단어가 아니거나 완성된 한글이면 중단
       const kr = krToEn(word); // 영어로 변환
-      list += " " + kr;
       answer[word] = kr; // 리턴 객체에 삽입
     }
   }
-  // console.log(list);
-  // console.log(answer);
   return answer;
 };
 
 export default forgotConvert;
 
-// let num = 0;
-// const list = [];
-// for (let i in arr) {
-//   const kr = enToKr(arr[i]);
-//   if (kr.split("").every((spell) => isCompleteHangul(spell.charCodeAt(0)))) {
-//     console.log(kr, arr[i]);
-//     list.push(kr);
-//     num++;
-//   }
-// }
-
-// for (const i of KR_LIST) {
-//   forgotConvert(i);
-//   console.log(
-//     "================================================================"
-//   );
-// }
+forgotConvert("dkssud!!!! (akssktj), qksrkdnj. skfkd clsgowlwl dksgdmffo?");
